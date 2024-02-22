a37730922b283b760f88c4cf3808af128f4af0dc
Carlos Carvajal <carv0066@algonquinlive.com>
2024-02-21 22:45:15 -0500

    Completes the Assignment

---
+++ script.js
@@ -0,0 +1,27 @@
+//#1
+
+const emojiArray = ['\u{1F60A}', '\u{1F355}', '\u{26BD}', '\u{1F388}', '\u{1F3B6}', '\u{1F4DA}','\u{1F5A5}', '\u{26F0}', '\u{1F30A}', '\u{1F3A8}', '\u{1F3B8}', '\u{1F31E}'];
+
+// console.log(emojiArray);
+
+//#2
+const emojiSection = document.getElementById('emoji');
+
+const items = [];
+
+for (const emoji of emojiArray) {
+    items.push(`
+    <div class="each-emoji">
+        <div  class="emoji-style">${emoji}</div>
+        <code class="code">${emoji.codePointAt(0).toString(16)}</code>
+    </div>
+    `);
+}
+
+//#3
+
+emojiSection.innerHTML = emojiSection.innerHTML + items.join('');
+
+
+
+
